d5a77f50487c473781f45ea7f647cd25892e1a24
Quentin Perez <qperez@ocs.online.net>
2016-02-08 11:08:16 +0100

    Add ifBoolIsFalse/ifBoolIsTrue

--- encodeFunc.go
+++ encodeFunc.go
@@ -11,3 +11,23 @@ func ifStringIsNotEmpty(obj interface{}) (string, bool, error) {
 	}
 	return "", false, errors.New("this field should be a string")
 }
+
+func ifBoolIsFalse(obj interface{}) (string, bool, error) {
+	if val, ok := obj.(bool); ok {
+		if !val {
+			return "False", true, nil
+		}
+		return "", false, nil
+	}
+	return "", false, errors.New("this field should be a boolean")
+}
+
+func ifBoolIsTrue(obj interface{}) (string, bool, error) {
+	if val, ok := obj.(bool); ok {
+		if val {
+			return "True", true, nil
+		}
+		return "", false, nil
+	}
+	return "", false, errors.New("this field should be a boolean")
+}
